532bd5e1e2a49b97b4369f7a97337fb2e169e33f
smoky0408 <40046110+smoky0408@users.noreply.github.com>
2020-01-21 19:56:09 -0700

    drive code

---
+++ src/main/include/subsystems/DriveTrain.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <frc/Talon.h> 
+#include <frc/drive/DifferentialDrive.h>
+#include <frc2/command/Subsystem.h>
+#include <frc/kinematics/DifferentialDriveKinematics.h>
+#include <frc/kinematics/DifferentialDriveOdometry.h>
+
+class DriveTrain : public frc2::Subsystem
+{
+    public:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
