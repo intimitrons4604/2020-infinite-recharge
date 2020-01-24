7b31de3b996166be4ea44195510b7c164eccccfb
kashmtr <kashish@intimitrons.ca>
2020-01-23 20:04:00 -0700

    DriveTrain2020

--- src/main/include/subsystems/DriveTrain.h
+++ src/main/include/subsystems/DriveTrain.h
@@ -5,22 +5,27 @@
 #include <frc2/command/Subsystem.h>
 #include <frc/kinematics/DifferentialDriveKinematics.h>
 #include <frc/kinematics/DifferentialDriveOdometry.h>
+#include <units/units.h>
+#include <frc/geometry/Pose2d.h>
 
 class DriveTrain : public frc2::Subsystem
 {
-    public:
+  public:
+    DriveTrain();
+    void TankDrive(double leftSpeed, double righSpeed);
 
+    void KinematicsDrive(double xSpeed, double zRotation, units::meter_t trackWidth, bool isQuickTurn);
 
+    void OdometryDrive(double xSeed, double zRotation, units::meter_t x, units::meter_t y);
 
+    void Stop();
 
+   private: 
+    frc::Talon left_front{PWM::left_front};
+    frc::Talon left_back{PWM::left_back};
+    frc::SpeedControllerGroup left_motors;
+    
+    frc::Talon right_front{PWM::right_front};
+    frc::Talon right_back{PWM::right_back};
 
-
-
-
-
-
-
-
-
-
-}
+};
