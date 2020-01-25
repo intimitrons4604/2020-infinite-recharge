cc71ed42774a9b51f95bd2af09cb694717972292
Celina-c2005 <celina@intimitrons.ca>
2020-01-25 13:45:04 -0700

    made arcade drive code, not finished

--- src/main/cpp/subsystems/DriveTrain.cpp
+++ src/main/cpp/subsystems/DriveTrain.cpp
@@ -1,4 +1,25 @@
 #include "subsystems/DriveTrain.h"
 
+static constexpr double kSpeedLimit = 0.5;
+static const char* kSubSystemName = "DriveTrain";
 
 
+
+
+void DriveTrain::ArcadeDrive(double xSpeed, double zRotation, bool squareInputs = true)
+{
+    double xSpeedLimit = 0;
+    double zSpeedLImit = 0;
+    if (squareInputs = true)
+    {
+        xSpeedLimit = 0;
+        zSpeedLImit = 0;
+    }
+    if (squareInputs = false)
+    {
+        xSpeedLimit = xSpeed * kSpeedLimit;
+        zSpeedLImit = zRotation * kSpeedLimit;
+    } 
+
+   drive_motors.ArcadeDrive(xSpeedLimit, zSpeedLimit, false);
+}
