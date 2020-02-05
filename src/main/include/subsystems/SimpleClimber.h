30c86c8db992ff586406d94285ae59824b69874c
smoky0408 <40046110+smoky0408@users.noreply.github.com>
2020-02-04 18:33:55 -0700

    Trying to upload improved winch code

--- src/main/include/subsystems/SimpleClimber.h
+++ src/main/include/subsystems/SimpleClimber.h
@@ -38,4 +38,6 @@ class SimpleClimber : public frc2::SubsystemBase {
 
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
+  //tester
+  int x;
 };
