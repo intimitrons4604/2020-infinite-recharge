7a7981eed35116cac050ad8b8d6585bdf4d336da
elena-gy <elena@robotics.com>
2020-03-05 20:15:22 -0700

    +enum

--- src/main/include/controls/Controls.h
+++ src/main/include/controls/Controls.h
@@ -13,10 +13,17 @@ enum class ColourControls
   Stop
 };
 
+enum class BallBucketControls 
+{
+  
+
+}
+
 class Controls{
   public:
     ArcadeControls getDriveControls();
     ColourControls getColourControls();
+    BallBucketControls getBallBucketControls();
   private:
     frc::XboxController controller{controller::main_controller}; 
 };
